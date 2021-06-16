7db9335386523d0d6ebaf8793f011f77970383dc
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2021-06-16 12:13:22 +0200

    Update bas keymap

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/bas/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/bas/keymap.c
@@ -21,14 +21,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case CODECMT:
             if (record->event.pressed) {
-                // ```
-                SEND_STRING("``````");
+                SEND_STRING("```");
             } 
             break;
         case CODE:
             if (record->event.pressed) {
-                // `
-                SEND_STRING("``");
+                SEND_STRING("`");
             } 
             break;
     }
@@ -38,11 +36,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_5x6(
-     KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                        KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_APOS,
-     KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                        KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_MINS,
-     CODE   , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                        KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
-     CODECMT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                        KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_BSLASH,
-                       KC_EXLM, KC_QSM ,                                                            KC_PLUS, KC_EQL ,
+     KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                        KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS,
+     KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                        KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_QUOT,
+     CODE   , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                        KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_PIPE,
+     CODECMT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                        KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_BSLS,
+                       KC_EXLM, KC_QUES,                                                            KC_PLUS, KC_EQL ,
                                                   RAISE  , KC_SPC ,      KC_ENT , LOWER  ,
                                                   KC_LSFT, KC_LGUI,      KC_BSPC, KC_DEL ,
                                                   KC_LCTL, ALTTAB ,      KC_LSFT, KC_LALT
@@ -50,9 +48,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT_5x6(
      KC_TILD, KC_EXLM,  KC_AT , KC_HASH, KC_DLR , KC_PERC,                        KC_CIRC, KC_AMPR, KC_ASTR, KC_SLSH, KC_LPRN, KC_RPRN,
-     KC_ACUT, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, KC_LCBR, KC_RCBR,
+     _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, KC_LCBR, KC_RCBR,
      _______, _______, _______, _______, _______, _______,                        KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_LBRC, KC_RBRC,
-     _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, KC_LESS, KC_GRTR,
+     _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, KC_LT  , KC_GT  ,
                        _______, _______,                                                            _______, _______,
                                                   _______, _______,      _______, _______,
                                                   _______, _______,      _______, _______,
@@ -62,8 +60,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT_5x6(
      KC_F12 , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                        KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
-     KC_GRV , _______, _______, _______, _______, _______,                        RESET  , _______, _______, _______, KC_MUTE, KC_PIPE,
-     _______, _______, _______, _______, _______, _______,                        KC_HOME, KC_PGDN, KC_PGUP, KC_END , KC_VOLU, KC_BSLS,
+     KC_GRV , _______, _______, _______, _______, _______,                        RESET  , _______, _______, _______, KC_MUTE, _______,
+     _______, _______, _______, _______, _______, _______,                        KC_HOME, KC_PGDN, KC_PGUP, KC_END , KC_VOLU, _______,
      _______, _______, _______, _______, _______, _______,                        _______, KC_MPLY, KC_MPRV, KC_MNXT, KC_VOLD, _______,
                        _______, _______,                                                            _______, _______, 
                                                   _______, _______,      _______, _______,
